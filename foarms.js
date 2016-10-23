d7f9d9f2164dfe41ba453e2798a7fbd5e50a10f1
RAYCHEV <nikolay.g.raychev@gmail.com>
2016-10-23 23:13:54 +0100

    add some files

---
+++ foarms.js
@@ -0,0 +1,13 @@
+    // my code here
+    for(var i = 10; i<20; i++){
+        console.log(i);
+    }
+    console.log("onLoded")
+
+window.onload = function () {
+    // my code here
+    for(var i = 10; i<20; i++){
+        console.log(i);
+    }
+    console.log("onLoded")
+}
